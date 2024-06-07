e4cbb5062f0431bb1e71abaf8889eefe8ce30c84
Sushant Bhutani <sb.bhutani12@gmail.com>
2024-06-08 00:08:55 +0530

    selection sort

---
+++ selectionSort.js
@@ -0,0 +1,19 @@
+// sort in increasing order
+const selectionSort = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let smallestNumberIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[smallestNumberIndex]) {
+        smallestNumberIndex = j;
+      }
+    }
+    if (arr[i] !== arr[smallestNumberIndex]) {
+      const temp = arr[smallestNumberIndex];
+      arr[smallestNumberIndex] = arr[i];
+      arr[i] = temp;
+    }
+  }
+  return arr;
+};
+
+console.log(selectionSort([5, 4, 3, 2, 1]));
