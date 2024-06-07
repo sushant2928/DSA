a6657c4d1fb70e48ee296f966afeab1098fefae4
Sushant Bhutani <sb.bhutani12@gmail.com>
2024-06-08 01:41:16 +0530

    bubble sort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,7 +1,7 @@
 // sort in increasing order
 // TC : O(n^2)
 // SC : O(1)
-const bubbleSort = (arr) => {
+const bubbleSortI = (arr) => {
   for (let i = arr.length - 2; i >= 0; i--) {
     // length 5 -> loop 4 times
     let swapped = false;
@@ -21,4 +21,27 @@ const bubbleSort = (arr) => {
   return arr;
 };
 
-console.log(bubbleSort([0, 7, 4, 8, 5, 3]));
+// sort in decreasing order
+// TC : O(n^2)
+// SC : O(1)
+const bubbleSortD = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let swapped = false;
+    for (j = arr.length - 1; j > i; j--) {
+      if (arr[j] > arr[j - 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j - 1];
+        arr[j - 1] = temp;
+        swapped = true;
+      }
+    }
+    if (!swapped) {
+      break;
+    }
+  }
+  return arr;
+};
+
+console.log(bubbleSortI([0, 7, 4, 8, 5, 3]));
+
+console.log(bubbleSortD([0, 7, 4, 8, 5, 3]));
