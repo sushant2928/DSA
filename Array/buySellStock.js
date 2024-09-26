14467189d7f4b8f22f1e9299132c3cae166901b8
Sushant Bhutani <sushant.bhutani@avisbudget.com>
2024-09-27 00:48:37 +0530

    better solution added for buy and sell stock

--- Array/buySellStock.js
+++ Array/buySellStock.js
@@ -3,7 +3,7 @@
  * @param {number[]} prices
  * @return {number}
  * 
- * brute force
+ * brute force - O(N^2)
  */
 var maxProfit = function (prices) {
     let maxProfit = 0
@@ -18,3 +18,29 @@ var maxProfit = function (prices) {
 
 console.log(maxProfit([7, 1, 5, 3, 6, 4]))
 console.log(maxProfit([7, 6, 4, 3, 1]))
+
+/**
+ * better solution - O(n)
+ */
+
+/**
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit2 = function (prices) {
+    let maxProfit = 0
+    let i = 0, j = 1;
+    while (i < j && j < prices.length) {
+        let diff = prices[j] - prices[i]
+        if (diff > maxProfit) maxProfit = diff
+        if (diff < 0) {
+            i = j
+        }
+        j += 1
+    }
+    return maxProfit
+};
+
+console.log(maxProfit2([7, 1, 5, 3, 6, 4]))
+console.log(maxProfit2([7, 6, 4, 3, 1]))
