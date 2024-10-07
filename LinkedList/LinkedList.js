7ae635a960d0684bef14961d965b8412cfb172af
Sushant Bhutani <sushant.bhutani@avisbudget.com>
2024-10-08 01:58:16 +0530

    Added more functions in linked list

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -1,3 +1,4 @@
+'use strict'
 class Node {
     constructor(nodeOrvalue) {
         if (nodeOrvalue instanceof Node) {
@@ -51,11 +52,68 @@ class LinkedList {
         }
     }
 
+    deleteFirst() {
+        const temp = this.#head
+        this.#head = this.#head?.next
+        temp.next = null
+    }
+
+    deleteLast() {
+        if (this.isEmpty()) return
+        if (this.#head === this.#tail) {
+            this.#head = this.#tail = null
+            return
+        }
+        let secondLast = this.#head
+        while (secondLast.next !== this.#tail) {
+            secondLast = secondLast.next
+        }
+        secondLast.next = null
+        this.#tail = secondLast
+    }
+
+    deleteAt(index) {
+        if (this.isEmpty()) throw new RangeError("Index out of bound!")
+        if (index === 0) {
+            this.deleteFirst()
+            return
+        }
+        let currentIndex = 0
+        let current = this.#head
+        let prev = null
+        while (currentIndex < index && current !== null) {
+            prev = current
+            current = current.next
+            currentIndex += 1
+        }
+        if (currentIndex < index) throw new RangeError("Index out of bound!")
+        if (current === this.#tail) {
+            this.deleteLast()
+            return
+        }
+        prev.next = current.next
+        current.next = null
+    }
+
+    deleteAtFromEnd(index) {
+        if (this.isEmpty()) throw new RangeError("Index out of bound!")
+        if (index === 0) {
+            this.deleteLast()
+            return
+        }
+        let total = 1
+        let current = this.#head
+        while (current.next) {
+            total += 1
+            current = current.next
+        }
+        this.deleteAt(total - index - 1)
+    }
 
     indexOf(value) {
         let current = new Node(this.#head)
         let index = -1
-        while (current?.value) {
+        while (current?.value === 0 || current?.value) {
             index += 1
             if (current.value === value) {
                 return index
@@ -66,14 +124,30 @@ class LinkedList {
         return -1
     }
 
+    reverse() {
+        if (!this.#head || !this.#head.next) {
+            return this
+        }
+        let prev = { ...this.#head }
+        let current = { ...prev?.next }
+        this.#tail = prev
+        this.#tail.next = null
+        while (current) {
+            this.#head = current
+            const next = current?.next
+            current.next = prev
+            prev = current
+            current = next
+        }
+    }
     toString() {
         let current = new Node(this.#head)
         let linkedListString = ""
-        while (current?.value) {
+        while (current?.value === 0 || current?.value) {
             linkedListString = linkedListString ? linkedListString + " --> " + String(current.value) : String(current.value)
             current = current.next
         }
-        return linkedListString
+        return linkedListString || "EMPTY"
     }
 
     isEmpty() {
@@ -82,17 +156,23 @@ class LinkedList {
 }
 
 const linkedList = new LinkedList()
-linkedList.insertAtFirst(9)
-linkedList.insert(1)
-linkedList.insert(2)
-linkedList.insert(2)
-linkedList.insert(4)
-linkedList.insertAtFirst(10)
 linkedList.insert(2)
 linkedList.insert(3)
-linkedList.insertAt(5, 9999)
-linkedList.insertAt(8, 999)
-linkedList.insertAt(2, 99)
+linkedList.insertAtFirst(1)
+linkedList.insert(4)
+linkedList.insert(5)
+linkedList.deleteAt(2)
+linkedList.insertAtFirst(0)
+linkedList.insert(6)
+linkedList.insert(7)
+linkedList.insertAt(5, 55)
+linkedList.reverse()
 console.log(linkedList.toString())
-console.log(linkedList.indexOf(999))
-console.log(linkedList.indexOf(2))
+linkedList.insertAt(1, 11)
+linkedList.deleteFirst()
+linkedList.deleteLast()
+console.log(linkedList.toString())
+linkedList.deleteAtFromEnd(3)
+console.log(linkedList.toString())
+console.log(linkedList.indexOf(5))
+
